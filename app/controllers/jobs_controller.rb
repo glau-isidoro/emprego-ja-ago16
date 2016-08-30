cb07a68663cd386682b25cd06366060ff97fe1b2
flavio_e_glaucia <glaucia.isidoro@gmail.com>
2016-08-30 20:40:01 -0300

    Usuario cria vaga

---
+++ app/controllers/jobs_controller.rb
@@ -0,0 +1,22 @@
+class JobsController < ApplicationController
+
+  def show
+    @job = Job.find(params[:id])
+  end
+
+  def new
+    @job = Job.new
+  end
+
+  def create
+    @job = Job.new(job_parameters)
+    @job.save
+
+    redirect_to @job
+  end
+end
+
+private
+  def job_parameters
+    params.require(:job).permit(:title, :location, :category, :company,:description)
+  end
