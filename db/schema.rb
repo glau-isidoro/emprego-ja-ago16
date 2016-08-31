169a7d3fce70eee8481b531c7499175b8e0be15e
flavio_e_glaucia <glaucia.isidoro@gmail.com>
2016-08-30 22:00:45 -0300

    [wip] Usuario usa empresas cadastradas

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160830235441) do
+ActiveRecord::Schema.define(version: 20160831005325) do
+
+  create_table "companies", force: :cascade do |t|
+    t.string   "name"
+    t.string   "location"
+    t.string   "mail"
+    t.string   "phone"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "jobs", force: :cascade do |t|
     t.string   "title"
